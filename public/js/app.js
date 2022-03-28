eacf871f3f5994039d7c97bbd75a7c215cbe4e3a
YossraElhaddad <nspecial56@gmail.com>
2022-03-29 01:20:01 +0200

    Set up the environment for hiroku (hopefully last time)

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ const result = document.querySelector('.result');
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault();
   const location = search.value;
-  fetch('https://weather-app-0022.herokuapp.com/weather?address=' + location).then((response)=>{
+  fetch('/weather?address=' + location).then((response)=>{
     response.json().then((data)=>{
       if(data.error)
       result.textContent = data.error;
